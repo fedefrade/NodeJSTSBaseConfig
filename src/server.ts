72e0fba12ea99cf866f34b9845069e50969c94fc
Federico Frade <federico.frade@mercadolibre.com>
2019-04-28 20:03:15 -0300

    app with express

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+let app: express.Application;
+
+function startApp() {
+  app = express();
+  app.use(bodyParser.json());
+
+
+  // Routes
+  app.get('/ping', (
+    req: express.Request,
+    res: express.Response,
+  ) => res.send('pong'));
+
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
+}
+
+startApp();
+export default app;
