4eab444fa5fe76d90bfcc73457421090bf310bf1
Federico Frade <fedefrade21@gmail.com>
2019-05-04 13:05:08 -0300

    Fix linter problems

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 let app: express.Application;
 
@@ -11,14 +11,14 @@ function startApp() {
   app = express();
   app.use(bodyParser.json());
 
-
   // Routes
-  app.get('/ping', (
+  app.get("/ping", (
     req: express.Request,
     res: express.Response,
-  ) => res.send('pong'));
+  ) => res.send("pong"));
 
   app.listen(PORT, HOST);
+  // eslint-disable-next-line no-console
   console.log(`Running on http://${HOST}:${PORT}`);
 }
 
